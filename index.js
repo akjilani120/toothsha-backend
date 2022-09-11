bcddb81313e750c37adad628e6ab75de2df38364
unknown <abdulkaderjilani120@gmail.com>
2022-09-12 00:24:53 +0600

    chance password

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors())
 app.use(express.json())
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bbiqawj.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://akjilani:vW5cGJMU0mIXxycJ@cluster0.bbiqawj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
